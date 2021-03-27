d3c47ad60bd44265ae9ce08aac8f4c298e98f9de
Soorria Saruva <soorria.ss@gmail.com>
2021-03-28 10:16:24 +1100

    Add meta tags for favicons and page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,12 @@ class MyDocument extends NextDocument {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#282a36" />
+          <meta name="theme-color" content="#282a36" />
           <link
             rel="preload"
             href="/fonts/poppins-regular-latin.woff2"
