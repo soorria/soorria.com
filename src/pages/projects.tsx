2d678281fb95fe0cee00b2279a6b19537eb8d3fa
Soorria Saruva <soorria.ss@gmail.com>
2021-03-27 19:29:42 +1100

    add projects page

---
+++ src/pages/projects.tsx
@@ -0,0 +1,14 @@
+import Container from '@/components/Container'
+import { PostHeading } from '@/components/PostLayout'
+
+interface ProjectsPageProps {}
+
+const ProjectsPage: React.FC<ProjectsPageProps> = () => {
+  return (
+    <Container>
+      <PostHeading>Projects</PostHeading>
+    </Container>
+  )
+}
+
+export default ProjectsPage
