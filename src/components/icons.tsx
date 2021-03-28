20458032d73fbcdf5391b041c47b9006c10cbe8f
Soorria Saruva <soorria.ss@gmail.com>
2021-03-28 18:29:40 +1100

    add triangle icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -35,3 +35,19 @@ export const ClockIconSolid: IconComponent = ({ className }) => (
     />
   </svg>
 )
+
+export const TriangleIcon: IconComponent = ({ className }) => (
+  <svg
+    className={className}
+    viewBox="0 0 24 24"
+    width="24"
+    height="24"
+    stroke="currentColor"
+    strokeWidth="2"
+    fill="none"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z"></path>
+  </svg>
+)
