60ab22eca917726a7adec658111f94e06d273fe3
Soorria Saruva <soorria.ss@gmail.com>
2021-04-02 13:07:12 +1100

    remove unused files

--- src/utils/mdx-hydrate.ts
+++
@@ -1,8 +0,0 @@
-import { MdxRemote } from 'next-mdx-remote/types'
-import { ReactNode } from 'react'
-import mdxHydrate from 'next-mdx-remote/hydrate'
-import { components } from 'src/components/MDXComponents'
-
-export const hydrate = (source: MdxRemote.Source): ReactNode => {
-  return mdxHydrate(source, { components })
-}
