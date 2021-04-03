9ada9f752efe7309fe0fa24ceff3288d7a2e5824
Soorria Saruva <soorria.ss@gmail.com>
2021-04-03 21:41:23 +1100

    always use default if it's broken

--- src/pages/api/og.ts
+++ src/pages/api/og.ts
@@ -4,31 +4,31 @@ import fs from 'fs'
 import path from 'path'
 
 const handler: NextApiHandler = async (req, res) => {
-  const { category, title, base } = req.query
-
-  if (
-    typeof category !== 'string' ||
-    (title && typeof title !== 'string') ||
-    typeof base !== 'string'
-  ) {
-    return res.status(406).end()
-  }
-
-  // This doesn't work sometimes. Not sure why.
-  // It says they node_modules/playwrite-core/browsers.json
-  // does not exist?????
-  //
-  // For some reason it works when I have this try/catch thing.
-  // Maybe it's the path.join / fs.readdir stuff
-
-  // For some reason this fixes it. What the fuck
-  const fileThatDoesntExistSometimes = path.join(
-    process.cwd(),
-    './node_modules/playwright-core/browsers.json'
-  )
-  fileThatDoesntExistSometimes
-
   try {
+    const { category, title, base } = req.query
+
+    if (
+      typeof category !== 'string' ||
+      (title && typeof title !== 'string') ||
+      typeof base !== 'string'
+    ) {
+      throw 1
+    }
+
+    // This doesn't work sometimes. Not sure why.
+    // It says they node_modules/playwrite-core/browsers.json
+    // does not exist?????
+    //
+    // For some reason it works when I have this try/catch thing.
+    // Maybe it's the path.join / fs.readdir stuff
+
+    // For some reason this fixes it. What the fuck
+    const fileThatDoesntExistSometimes = path.join(
+      process.cwd(),
+      './node_modules/playwright-core/browsers.json'
+    )
+    fileThatDoesntExistSometimes
+
     const browser = await playwright.launchChromium()
     const page = await browser.newPage({
       viewport: {
