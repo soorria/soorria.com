6dc72c7beb4b2ebc1fe8a409199912d2c2b93ada
Soorria Saruva <soorria.ss@gmail.com>
2021-04-03 18:39:40 +1100

    change project card to how it was before copying snippetCard

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,56 +1,57 @@
 import { ProjectFrontMatter } from '@/types/project'
 import Link from 'next/link'
-import { MouseEventHandler } from 'react'
-import { CodeIcon, ExternalIcon } from './icons'
+import { CodeIcon, ExternalIcon, InfoIcon } from './icons'
 
 interface ProjectCardProps {
   project: ProjectFrontMatter
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
-  const handleInnerLinkClick: MouseEventHandler = event => {
-    event.preventDefault()
-  }
-
   return (
-    <Link href={`/projects/${project.slug}`}>
-      <a className="flex flex-col px-4 py-3 space-y-3 transition-shadow rounded-xl md:py-6 md:px-8 bg-drac-curr hover:shadow-2xl group focus:ring-2 ring-drac-pink">
-        <div className="text-xl font-bold text-drac-pink font-display group-hover:underline">
-          {project.title}
+    <article className="flex flex-col space-y-3">
+      <div className="text-xl font-bold text-drac-pink font-display">{project.title}</div>
+      <div className="flex-1">{project.short_description}</div>
+      <div>
+        <div className="flex flex-wrap -mt-1 -ml-2 text-xs text-opacity-80 text-drac-fg">
+          {project.stack.map(tech => (
+            <span key={tech} className="mt-1 ml-2">
+              {tech}
+            </span>
+          ))}
         </div>
-        <div className="flex-1">{project.short_description}</div>
-        <div>
-          <div className="flex flex-wrap -mt-1 -ml-2 text-sm text-opacity-80 text-drac-fg">
-            {project.stack.map(tech => (
-              <span key={tech} className="mt-1 ml-2">
-                {tech}
-              </span>
-            ))}
-          </div>
-        </div>
-        <div className="flex space-x-4 text-sm">
+      </div>
+      <div className="flex space-x-4 text-sm">
+        <Link href={`/projects/${project.slug}`}>
           <a
-            href={project.live}
+            aria-label={`See details for ${project.title}`}
             className="inline-flex items-center space-x-1 underline text-drac-pink hover:text-drac-purple"
-            onClick={handleInnerLinkClick}
           >
-            <ExternalIcon className="inline-block w-4 h-4" />
-            <span>Live Demo</span>
+            <InfoIcon className="inline-block w-4 h-4" />
+            <span>Details</span>
           </a>
+        </Link>
 
-          {project.source && (
-            <a
-              href={project.source}
-              className="inline-flex items-center space-x-1 underline text-drac-pink hover:text-drac-purple"
-              onClick={handleInnerLinkClick}
-            >
-              <CodeIcon className="inline-block w-4 h-4" />
-              <span>Source</span>
-            </a>
-          )}
-        </div>
-      </a>
-    </Link>
+        <a
+          href={project.live}
+          aria-label={`View the live site for ${project.title}`}
+          className="inline-flex items-center space-x-1 underline text-drac-pink hover:text-drac-purple"
+        >
+          <ExternalIcon className="inline-block w-4 h-4" />
+          <span>View Live</span>
+        </a>
+
+        {project.source && (
+          <a
+            href={project.source}
+            aria-label={`View the source code for ${project.title}`}
+            className="inline-flex items-center space-x-1 underline text-drac-pink hover:text-drac-purple"
+          >
+            <CodeIcon className="inline-block w-4 h-4" />
+            <span>Source</span>
+          </a>
+        )}
+      </div>
+    </article>
   )
 }
 
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,14 +1,54 @@
 import MainLayout from '@/components/MainLayout'
 import { PostHeading } from '@/components/PostLayout'
+import ProjectCard from '@/components/ProjectCard'
+import { getAllFilesFrontMatter } from '@/lib/data'
+import { DataType } from '@/types/data'
+import { ProjectFrontMatter } from '@/types/project'
+import { GetStaticProps } from 'next'
+import { NextSeo } from 'next-seo'
 
-interface ProjectsPageProps {}
+interface ProjectsPageProps {
+  projects: ProjectFrontMatter[]
+}
 
-const ProjectsPage: React.FC<ProjectsPageProps> = () => {
+const description = "Things I've made. Some are more useful and cooler than others."
+const title = 'Projects'
+const url = 'https://mooth.tech/projects'
+
+const ProjectsPage: React.FC<ProjectsPageProps> = ({ projects }) => {
   return (
     <MainLayout>
+      <NextSeo
+        description={description}
+        title={title}
+        canonical={url}
+        openGraph={{ title, description, type: 'website', url }}
+      />
       <PostHeading>Projects</PostHeading>
+      <p className="mt-6 mb-12 text-lg text-center">{description}</p>
+      <div className="grid grid-cols-1 gap-12 md:grid-cols-2 auto-cols-min">
+        {projects.map(project => (
+          <ProjectCard key={project.slug} project={project} />
+        ))}
+      </div>
     </MainLayout>
   )
 }
 
 export default ProjectsPage
+
+const projectOrder = ['jupyter-js', 'not-messenger', 'clinically-relevant', 'mooth-tech']
+const getProjectIndex = (slug: string): number => {
+  const idx = projectOrder.indexOf(slug)
+  return idx >= 0 ? idx : projectOrder.length + 10
+}
+
+export const getStaticProps: GetStaticProps<ProjectsPageProps> = async () => {
+  const projects = (await getAllFilesFrontMatter<ProjectFrontMatter>(DataType.projects)).sort(
+    (a, b) => getProjectIndex(a.slug) - getProjectIndex(b.slug)
+  )
+
+  return {
+    props: { projects },
+  }
+}
