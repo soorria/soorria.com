cecfca9e4a4b366cafe8f253a0676daae70b8b38
Soorria Saruva <soorria.ss@gmail.com>
2021-04-03 21:11:52 +1100

    slightly better error stuff for testing

--- src/pages/api/og.ts
+++ src/pages/api/og.ts
@@ -10,30 +10,34 @@ const handler: NextApiHandler = async (req, res) => {
     return res.status(406).end()
   }
 
-  console.log({
+  const errResult = {
     files: fs.readdirSync(path.join(process.cwd(), './node_modules/playwright-core/')),
-  })
+  }
 
-  const browser = await playwright.launchChromium()
-  const page = await browser.newPage({
-    viewport: {
-      width: 1200,
-      height: 600,
-    },
-  })
+  try {
+    const browser = await playwright.launchChromium()
+    const page = await browser.newPage({
+      viewport: {
+        width: 1200,
+        height: 600,
+      },
+    })
 
-  const url = `${base}/og/${category}/${encodeURIComponent(title)}`
+    const url = `${base}/og/${category}/${encodeURIComponent(title)}`
 
-  await page.goto(url, { timeout: 15 * 1000 })
-  const data = await page.screenshot({
-    type: 'png',
-  })
-  await browser.close()
+    await page.goto(url, { timeout: 15 * 1000 })
+    const data = await page.screenshot({
+      type: 'png',
+    })
+    await browser.close()
 
-  res.setHeader('Cache-Control', 's-maxage=31536000, stale-while-revalidate')
-  res.setHeader('Content-Type', 'image/png')
+    res.setHeader('Cache-Control', 's-maxage=31536000, stale-while-revalidate')
+    res.setHeader('Content-Type', 'image/png')
 
-  res.end(data)
+    res.end(data)
+  } catch (err) {
+    res.json({ ...errResult, err })
+  }
 }
 
 export default handler
