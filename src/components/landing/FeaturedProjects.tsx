2c6d21d5b1e45e9d6e0acc9eda1bcb0885e4668c
Soorria Saruva <soorria.ss@gmail.com>
2021-04-03 19:09:14 +1100

    Add link to all projects in featured projects section

--- src/components/landing/FeaturedProjects.tsx
+++ src/components/landing/FeaturedProjects.tsx
@@ -1,4 +1,5 @@
 import { ProjectFrontMatter } from '@/types/project'
+import Link from 'next/link'
 import ProjectCard from '../ProjectCard'
 
 interface FeaturedProjectsProps {
@@ -9,10 +10,17 @@ const FeaturedProjects: React.FC<FeaturedProjectsProps> = ({ projects }) => {
   return (
     <section className="my-8">
       <h3 className="mb-8 text-4xl font-bold font-display">Projects</h3>
-      <div className="grid grid-cols-1 gap-12 sm:grid-cols-2">
+      <div className="grid grid-cols-1 gap-12 mb-8 sm:grid-cols-2">
         {projects.map(project => (
           <ProjectCard key={project.slug} project={project} />
         ))}
+      </div>
+      <div className="text-center">
+        <Link href="/projects" passHref>
+          <a className="text-drac-pink hover:text-drac-purple">
+            <span>See All Projects</span>
+          </a>
+        </Link>
       </div>
     </section>
   )
