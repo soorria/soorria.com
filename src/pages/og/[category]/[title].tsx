b570e69e1defb14713442edc339a1db35db7bf82
Soorria Saruva <soorria.ss@gmail.com>
2021-04-04 11:45:51 +1000

    update og image template

--- src/pages/og/[category]/[title].tsx
+++ src/pages/og/[category]/[title].tsx
@@ -14,7 +14,7 @@ const OGCategoryImagePage: React.FC<OGCategoryImagePageProps> = () => {
       </div>
       <div className="relative flex items-center justify-center h-full text-center font-display">
         <div className="flex flex-col justify-center p-16 min-w-[600px] bg-drac-bg bg-opacity-70 text-drac-fg rounded-3xl item-center">
-          <p className="mb-4 text-5xl font-bold capitalize">{router.query.title}</p>
+          <p className="mb-4 text-5xl font-bold">{router.query.title}</p>
           <p className="text-3xl">
             <span className="capitalize">{router.query.category}</span> | mooth.tech
           </p>
