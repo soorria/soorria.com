f118451735c32bcb5def4967fc2b38b98946fd75
Soorria Saruva <soorria.ss@gmail.com>
2021-04-04 17:36:33 +1000

    Add bg behind contact section

--- src/components/landing/Contact.tsx
+++ src/components/landing/Contact.tsx
@@ -33,26 +33,29 @@ const LINKS = [
 
 const Contact: React.FC<ContactProps> = () => {
   return (
-    <div className="px-4 py-6 sm:p-8 bg-drac-curr rounded-3xl">
-      <h3 className="mb-6 text-4xl font-bold font-display">Contact</h3>
-      <div className="grid grid-cols-1 gap-8 text-xl sm:grid-cols-2">
-        <div className="">
-          The best way to get in touch is probably to email me or shoot me a message on Facebook
-          Messenger.
-        </div>
-        <div className="flex flex-col space-y-4">
-          {LINKS.map(({ href, title, Icon }) => (
-            <a
-              key={title}
-              href={href}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center text-drac-pink hover:text-drac-purple group"
-            >
-              <Icon className="w-5 h-5 mr-2 transition-transform transform group-hover:-rotate-12" />
-              <span>{title}</span>
-            </a>
-          ))}
+    <div className="relative py-8 sm:py-16">
+      <div className="absolute top-0 w-screen h-full transform -translate-x-1/2 left-1/2 bg-drac-curr" />
+      <div className="relative">
+        <h3 className="mb-6 text-4xl font-bold font-display">Contact</h3>
+        <div className="grid grid-cols-1 gap-8 text-xl sm:grid-cols-2">
+          <div className="">
+            The best way to get in touch is probably to email me or shoot me a message on Facebook
+            Messenger.
+          </div>
+          <div className="flex flex-col space-y-4">
+            {LINKS.map(({ href, title, Icon }) => (
+              <a
+                key={title}
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center text-drac-pink hover:text-drac-purple group"
+              >
+                <Icon className="w-5 h-5 mr-2 transition-transform transform group-hover:-rotate-12" />
+                <span>{title}</span>
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </div>
