8d817e6144678e147e06454109b0287e4581a3fb
Soorria Saruva <soorria.ss@gmail.com>
2021-04-05 10:20:22 +1000

    update contact section to use LandingSection

--- src/components/landing/Contact.tsx
+++ src/components/landing/Contact.tsx
@@ -6,6 +6,7 @@ import {
   LinkedinIconSolid,
   TwitterIconSolid,
 } from '../icons'
+import LandingSection from './LandingSection'
 
 interface ContactProps {}
 
@@ -39,31 +40,28 @@ const LINKS = [
 
 const Contact: React.FC<ContactProps> = () => {
   return (
-    <div className="relative py-8 sm:py-16" id="contact">
-      <div className="relative">
-        <h3 className="mb-6 text-4xl font-bold font-display">Contact</h3>
-        <div className="grid grid-cols-1 text-xl gap-y-8 gap-x-16 sm:grid-cols-2">
-          <div className="">
-            The best way to get in touch is probably to email me or shoot me a message on Facebook
-            Messenger.
-          </div>
-          <div className="flex flex-col space-y-4">
-            {LINKS.map(({ href, title, Icon }) => (
-              <a
-                key={title}
-                href={href}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center text-drac-pink hover:text-drac-purple group"
-              >
-                <Icon className="w-5 h-5 mr-2 transition-transform transform group-hover:-rotate-12" />
-                <span>{title}</span>
-              </a>
-            ))}
-          </div>
+    <LandingSection title="Contact" id="contact">
+      <div className="grid grid-cols-1 text-xl gap-y-8 gap-x-16 sm:grid-cols-2">
+        <div>
+          The best way to get in touch is probably to email me or shoot me a message on Facebook
+          Messenger.
+        </div>
+        <div className="flex flex-col space-y-4">
+          {LINKS.map(({ href, title, Icon }) => (
+            <a
+              key={title}
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center text-drac-pink hover:text-drac-purple group"
+            >
+              <Icon className="w-5 h-5 mr-2 transition-transform transform group-hover:-rotate-12" />
+              <span>{title}</span>
+            </a>
+          ))}
         </div>
       </div>
-    </div>
+    </LandingSection>
   )
 }
 
