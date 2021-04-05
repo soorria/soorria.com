356aad403294ad66148fafcd4c1ef3c9747f9b5f
Soorria Saruva <soorria.ss@gmail.com>
2021-04-05 13:03:26 +1000

    add robots.txt and google site tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,10 @@ class MyDocument extends NextDocument {
         <Head>
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="google-site-verification"
+            content="Cl0BjsWegjV0EoEmhPMVdyI9qWoAdOwh5S-h37tEaao"
+          />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
