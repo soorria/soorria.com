1398c0596274e2b358529b4fae0b3313a3b05801
Soorria Saruva <soorria.ss@gmail.com>
2021-04-28 10:02:42 +1000

    Try add plausible

--- src/hooks/useRegisterView.ts
+++
@@ -1,7 +0,0 @@
-import { DataType } from 'src/types/data'
-
-const useRegisterView = (type: DataType, slug: string) => {
-  console.log(type, slug)
-}
-
-export default useRegisterView
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,12 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { DefaultSeo } from 'next-seo'
 import { SEO } from '@/next-seo.config'
+import PlausibleProvider from 'next-plausible'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   if (pageProps.noBoilerPlate) return <Component {...pageProps} />
   return (
-    <>
+    <PlausibleProvider domain="mooth.tech" customDomain="https://plausible.mooth.tech" selfHosted>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
@@ -20,7 +21,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
         <Component {...pageProps} />
       </main>
       <Footer />
-    </>
+    </PlausibleProvider>
   )
 }
 
