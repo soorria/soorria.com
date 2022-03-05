6ef61fc46298662276b613f276c37016de452575
Soorria Saruva <soorria.ss@gmail.com>
2022-03-05 20:40:29 +1100

    undo mdx section changes

--- src/components/mdx/utils.ts
+++ src/components/mdx/utils.ts
@@ -1,3 +1,3 @@
 export const COMMON_CLASSNAMES = {
-  codeAndDemoRoot: 'relative -mx-4 md:rounded p-4 ring-2 ring-drac-purple md:-mx-6 md:py-4 md:px-6',
+  codeAndDemoRoot: 'relative -mx-2 rounded p-2 ring-2 ring-drac-purple md:-mx-6 md:py-4 md:px-6',
 }
