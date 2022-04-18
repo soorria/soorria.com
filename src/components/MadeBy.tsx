3016b4140ed4207e25f3a2000d24286e20686ba8
Soorria Saruva <soorria.ss@gmail.com>
2022-04-18 21:16:31 +1000

    add footer link

--- src/components/MadeBy.tsx
+++ src/components/MadeBy.tsx
@@ -1,17 +1,21 @@
+import Link from 'next/link'
+import { PUBLIC_URL } from '@/constants'
 import { HeartIcon } from './icons'
 
 interface MadeByProps {}
 
 const MadeBy: React.FC<MadeByProps> = () => {
   return (
-    <div className="group text-center">
-      Made with{' '}
-      <span className="relative inline-block h-5 w-5 align-middle">
-        <HeartIcon className="absolute inset-0 fill-current text-drac-purple" />
-        <HeartIcon className="absolute inset-0 fill-current text-drac-pink group-hover:animate-ping" />
-      </span>{' '}
-      by Soorria
-    </div>
+    <Link href={PUBLIC_URL}>
+      <a className="group block text-center">
+        Made with{' '}
+        <span className="relative inline-block h-5 w-5 align-middle">
+          <HeartIcon className="absolute inset-0 fill-current text-drac-purple" />
+          <HeartIcon className="absolute inset-0 fill-current text-drac-pink group-hover:animate-ping" />
+        </span>{' '}
+        by <span className="hover:underline">Soorria</span>
+      </a>
+    </Link>
   )
 }
 
