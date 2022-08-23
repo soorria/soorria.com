88911e861cb3b988a28c66e593b65e809cef0e2e
Soorria Saruva <soorria.ss@gmail.com>
2022-08-23 23:48:36 +1000

    update edit url

--- src/utils/editUrl.ts
+++ src/utils/editUrl.ts
@@ -2,6 +2,6 @@ import type { DataType } from '@/types/data'
 import { GITHUB_URL } from '@/constants'
 
 const editUrl = (type: DataType, slug: string): string =>
-  `${GITHUB_URL}/soorria.com/edit/master/_data/${type}/${slug}.mdx`
+  `${GITHUB_URL}/soorria.com/edit/master/_data/${type}/${slug}/index.mdx`
 
 export default editUrl
