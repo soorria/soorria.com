4528ee58b4a69975abcb31d0ca4b4a4dc7c13881
Soorria Saruva <soorria.ss@gmail.com>
2022-09-03 11:44:02 +1000

    minor transition change

--- src/components/SnippetCard.tsx
+++ src/components/SnippetCard.tsx
@@ -12,7 +12,7 @@ const SnippetCard: React.FC<{ snippet: SnippetFrontMatter }> = ({
     <Link href={`/snippets/${slug}`} passHref>
       <a className="group relative overflow-hidden rounded-xl bg-drac-curr p-4 ring-drac-pink transition-shadow hover:shadow-2xl focus:outline-none focus:ring md:py-6 md:px-8 md:focus:ring-4">
         <div className="absolute inset-0 flex items-center text-drac-comment text-opacity-20">
-          <div className="h-48 w-48 -translate-x-6 scale-110 transition-transform group-hover:-rotate-20 group-focus:-rotate-12">
+          <div className="h-48 w-48 -translate-x-6 scale-110 group-hocus:-rotate-20 motion-safe:transition-transform">
             <Icon />
           </div>
         </div>
