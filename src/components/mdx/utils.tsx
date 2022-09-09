0df7dae23bff31c71c0a49588f5eae2c65b84ae4
Soorria Saruva <soorria.ss@gmail.com>
2022-09-09 22:39:04 +1000

    fix scrollbar track for code blocks

--- src/components/mdx/utils.tsx
+++ src/components/mdx/utils.tsx
@@ -3,7 +3,8 @@ import cx from '@/utils/cx'
 import { useCopy } from '@/utils/use-copy'
 
 export const COMMON_CLASSNAMES = {
-  codeAndDemoRoot: 'relative -mx-2 rounded p-2 ring-2 ring-drac-purple md:-mx-6 md:py-4 md:px-6',
+  codeAndDemoRoot:
+    'relative -mx-2 rounded p-2 ring-2 ring-drac-purple md:-mx-6 md:py-4 md:px-6 bg-drac-bg',
 } as const
 
 export const CODE_BLOCK_CLASSNAMES = {
