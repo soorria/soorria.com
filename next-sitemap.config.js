0ee8a726228ef56a9e32fd4794478ff3d9740594
Soorria Saruva <soorria.ss@gmail.com>
2022-09-11 10:26:49 +1000

    update sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://soorria.com',
+  generateRobotsTxt: true, // (optional)
+  generateIndexSitemap: false,
+  // ...other options
+}
