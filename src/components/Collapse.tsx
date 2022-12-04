b8c9b7da4208b0b5a901471ff121447618cdb830
Soorria Saruva <soorria.ss@gmail.com>
2022-12-04 16:30:24 +1100

    hide default <summary> icon in safari

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -12,8 +12,8 @@ const Collapse: React.FC<CollapseProps> = ({
   summaryComponent: SummaryComponent = 'h2',
 }) => {
   return (
-    <details className="group space-y-8 rounded bg-drac-base-dark p-4 transition-shadow">
-      <summary className="not-prose focus-ring -m-4 flex cursor-pointer appearance-none items-center space-x-4 rounded p-4">
+    <details className="group list-none space-y-8 rounded bg-drac-base-dark p-4 transition-shadow">
+      <summary className="not-prose focus-ring -m-4 flex cursor-pointer list-none appearance-none items-center space-x-4 rounded p-4 [&::-webkit-details-marker]:hidden">
         <TriangleIcon className="h-3 w-3 rotate-90 fill-white text-white transition-transform group-open:rotate-180" />
         <SummaryComponent className="ml-1 inline-block font-display font-bold">
           {summary}
