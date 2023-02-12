b251c87bb9ea590f2fd7c639a9ff4875f804b5f1
Soorria Saruva <soorria.ss@gmail.com>
2023-02-12 19:37:09 +1100

    fix link minimal mdx link styles

--- src/components/mdx/minimal.tsx
+++ src/components/mdx/minimal.tsx
@@ -1,6 +1,8 @@
 import { ComponentMap } from 'mdx-bundler/client'
 import dynamic from 'next/dynamic'
+import CustomLink from '../CustomLink'
 
 export const minimalComponents = {
   Sparkles: dynamic(() => import('./Sparkles')),
+  a: CustomLink,
 } as ComponentMap
