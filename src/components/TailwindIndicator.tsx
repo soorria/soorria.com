26f518842479fa59251cbb669f0186d583bd7234
Soorria Saruva <soorria.ss@gmail.com>
2023-06-25 10:46:15 +1000

    make tw indicator easier to read

--- src/components/TailwindIndicator.tsx
+++ src/components/TailwindIndicator.tsx
@@ -6,7 +6,7 @@ export function TailwindIndicator() {
   if (process.env.NODE_ENV === 'production') return null
 
   return (
-    <div className="fixed bottom-2 left-2 z-50 flex h-6 w-6 items-center justify-center rounded-full bg-drac-base-dark p-3 font-mono text-sm text-drac-pink">
+    <div className="fixed bottom-2 left-2 z-50 flex h-6 items-center justify-center rounded-full bg-drac-base-dark px-2 font-mono text-drac-pink">
       <div className="block sm:hidden">xs</div>
       <div className="hidden sm:block md:hidden">sm</div>
       <div className="hidden md:block lg:hidden">md</div>
