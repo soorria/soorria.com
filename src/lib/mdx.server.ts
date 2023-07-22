9143773c27aaf8612b7c1bdee38d45751c29b51a
Soorria Saruva <soorria.ss@gmail.com>
2023-07-22 14:59:14 +1000

    try importing dracula theme to trick next.js vercel build

--- src/lib/mdx.server.ts
+++ src/lib/mdx.server.ts
@@ -11,6 +11,8 @@ import { Element } from 'hast'
 import { rehypeRearrangePrettyCodeOutput } from './rehype.server'
 import { SerializeOptions } from 'next-mdx-remote/dist/types'
 
+import _draculaTheme from 'shiki/themes/dracula.json'
+
 const codeBlockRemarkPlugins: PluggableList = [remarkTypeScriptTransform]
 
 const codeBlockRehypePlugins: PluggableList = [
