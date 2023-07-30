a793f83e42d20b95b4d5bb9b5cdce72731dd28c2
Soorria Saruva <soorria.ss@gmail.com>
2023-07-30 19:35:02 +1000

    update date formatting

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,6 +1,6 @@
 export const formatDate = (date: Date | string | null): string =>
   date
-    ? new Date(date).toLocaleDateString(undefined, {
+    ? new Date(date).toLocaleDateString('en-GB', {
         year: '2-digit',
         month: '2-digit',
         day: '2-digit',
