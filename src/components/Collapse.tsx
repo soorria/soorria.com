263524c5d42265e975157c47d2f2fe93066f7c55
Soorria Saruva <soorria.ss@gmail.com>
2024-01-03 09:25:44 +1100

    feat: drizzle-orm types snippet

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -4,15 +4,20 @@ import { TriangleIcon } from './icons'
 export type CollapseProps = PropsWithChildren<{
   summary: ReactNode
   summaryComponent?: ElementType
+  id?: string
 }>
 
 const Collapse: React.FC<CollapseProps> = ({
   summary,
   children,
   summaryComponent: SummaryComponent = 'h2',
+  id,
 }) => {
   return (
-    <details className="group list-none space-y-8 rounded bg-drac-base-dark p-4 transition-shadow">
+    <details
+      className="group list-none space-y-8 rounded bg-drac-base-dark p-4 transition-shadow"
+      id={id}
+    >
       <summary className="not-prose focus-ring -m-4 flex cursor-pointer list-none appearance-none items-center space-x-4 rounded p-4 [&::-webkit-details-marker]:hidden">
         <TriangleIcon className="h-3 w-3 rotate-90 fill-white text-white transition-transform group-open:rotate-180" />
         <SummaryComponent className="ml-1 inline-block font-display font-bold">
