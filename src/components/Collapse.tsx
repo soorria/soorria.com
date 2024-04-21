03e980623163d85dd61fad8d6f3d987c4f8c8d37
Soorria Saruva <soorria.ss@gmail.com>
2024-04-21 22:33:10 +1000

    fix: code-block margin in <details>

--- src/components/Collapse.tsx
+++ src/components/Collapse.tsx
@@ -15,11 +15,11 @@ const Collapse: React.FC<CollapseProps> = ({
 }) => {
   return (
     <details
-      className="group list-none space-y-8 rounded bg-drac-base-dark p-4 transition-shadow"
+      className="group/collapse list-none space-y-8 rounded bg-drac-base-dark p-4 transition-shadow"
       id={id}
     >
       <summary className="not-prose focus-ring -m-4 flex cursor-pointer list-none appearance-none items-center space-x-4 rounded p-4 [&::-webkit-details-marker]:hidden">
-        <TriangleIcon className="h-3 w-3 rotate-90 fill-white text-white transition-transform group-open:rotate-180" />
+        <TriangleIcon className="h-3 w-3 rotate-90 fill-white text-white transition-transform group-open/collapse:rotate-180" />
         <SummaryComponent className="ml-1 inline-block font-display font-bold">
           {summary}
         </SummaryComponent>
