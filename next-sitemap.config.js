b7d7e5c331daef9b3044e27a345adb4643485110
Soorria Saruva <soorria.ss@gmail.com>
2024-05-21 23:11:16 +1000

    refactor: robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://soorria.com',
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: false,
   generateIndexSitemap: false,
   // ...other options
 }
