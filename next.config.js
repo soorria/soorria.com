00000002ce465115dce4c16113a225b11704dbf0
Soorria Saruva <soorria.ss@gmail.com>
2024-09-02 20:56:45 +1000

    chore: clean up next config

--- next.config.js
+++ next.config.js
@@ -7,11 +7,8 @@ const { FontaineTransform } = require('fontaine')
 
 const options = {
   fallbacks: ['BlinkMacSystemFont', 'Segoe UI', 'Helvetica Neue', 'Arial', 'Noto Sans'],
-  // You may need to resolve assets like `/fonts/Roboto.woff2` to a particular directory
   resolvePath: id => 'file://./public/fonts' + id,
 }
-
-// const withTM = require('next-transpile-modules')(['rehype-raw'])
 
 const { withPlausibleProxy } = require('next-plausible')
 
