0000000577f03d82651b1f4d108a00c450f41deb
Soorria Saruva <soorria.ss@gmail.com>
2025-02-12 22:55:04 +1100

    fix

--- astro-tmp/src/lib/ui/made-by.tsx
+++ astro-tmp/src/lib/ui/made-by.tsx
@@ -1,61 +1,50 @@
-import Link from "next/link";
-import { HeartIcon } from "./icons";
-import { cx } from "../utils/styles";
-import { PUBLIC_URL } from "../constants/site";
+import { HeartIcon } from './icons'
+import { cx } from '../utils/styles'
+import { PUBLIC_URL } from '../constants/site'
 
 interface MadeByProps {
-  asLink?: boolean | "external";
-  ping?: "always" | "hover";
+  asLink?: boolean | 'external'
+  ping?: 'always' | 'hover'
 }
 
 const MadeBy: React.FC<MadeByProps> = (props) => {
   const classes = {
     ping:
-      props.ping === "hover"
-        ? "group-hover:supports-hover:animate-ping-slow"
-        : "animate-ping-slow",
-    text: props.asLink ? "group-hover:supports-hover:underline" : "",
-  };
+      props.ping === 'hover' ? 'group-hover:supports-hover:animate-ping-slow' : 'animate-ping-slow',
+    text: props.asLink ? 'group-hover:supports-hover:underline' : '',
+  }
 
   const content = (
     <>
-      <span className={classes.text}>Made with</span>{" "}
+      <span className={classes.text}>Made with</span>{' '}
       <span className="relative inline-block h-5 w-5 align-middle">
-        <HeartIcon className="absolute inset-0 fill-current text-drac-purple" />
+        <HeartIcon className="text-drac-purple absolute inset-0 fill-current" />
         <HeartIcon
-          className={cx(
-            "absolute inset-0 fill-current text-drac-red",
-            classes.ping,
-          )}
-          style={{ animationDelay: "150ms" }}
+          className={cx('text-drac-red absolute inset-0 fill-current', classes.ping)}
+          style={{ animationDelay: '150ms' }}
         />
-        <HeartIcon
-          className={cx(
-            "absolute inset-0 fill-current text-drac-pink",
-            classes.ping,
-          )}
-        />
-      </span>{" "}
+        <HeartIcon className={cx('text-drac-pink absolute inset-0 fill-current', classes.ping)} />
+      </span>{' '}
       <span className={classes.text}>
         by <span>Soorria</span>
       </span>
     </>
-  );
+  )
 
   const commonProps = {
-    className: "focus-ring group block rounded-sm text-center",
-    "aria-label": "Made with love by Soorria",
-  };
+    className: 'focus-ring group block rounded-sm text-center',
+    'aria-label': 'Made with love by Soorria',
+  }
 
   if (props.asLink) {
     return (
-      <a href={props.asLink === "external" ? PUBLIC_URL : "/"} {...commonProps}>
+      <a href={props.asLink === 'external' ? PUBLIC_URL : '/'} {...commonProps}>
         {content}
       </a>
-    );
+    )
   }
 
-  return <p {...commonProps}>{content}</p>;
-};
+  return <p {...commonProps}>{content}</p>
+}
 
-export default MadeBy;
+export default MadeBy
