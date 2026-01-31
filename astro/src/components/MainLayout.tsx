9002147da93ab4796a375beff4a546c7da3f5c89
Soorria Saruva <soorria.ss@gmail.com>
2026-01-31 13:32:06 +1100

    feat: add additional layout and landing components
    - Add MainLayout component
    - Add Hero component and hero-patterns for landing pages
    - Add PostLayout and ProseWrapper for content pages

---
+++ astro/src/components/MainLayout.tsx
@@ -0,0 +1,12 @@
+import { type PropsWithChildren } from 'react'
+import Container from './Container'
+
+const MainLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <Container>
+      <div className="py-8">{children}</div>
+    </Container>
+  )
+}
+
+export default MainLayout
